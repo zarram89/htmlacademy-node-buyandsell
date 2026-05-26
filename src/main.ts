b0daaab782509cf68e78dc10c35fb422b97b6c71
Ramil <zarram89@gmail.com>
2026-05-26 15:47:35 +0500

    1.7. Добавит точку входа для CLI-приложения (`main.cli.ts`) Соберём всё вместе: создадим отдельную точку входа для CLI-приложения. В ней опишем единственную функцию `bootstrap` в которой выполним инициализацию приложения.
    Попробуйте протестировать приложение с помощью сценария `ts`. Например: `npm run ts ./src/main.cli.ts -- --version`. Обратите внимание на два дополнительных дефиса. Мы передаём параметры для `main.cli.ts`, а не для `npm`.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('hello');
+import { CLIApplication, HelpCommand, VersionCommand } from './cli/index.js';
+
+function bootstrap() {
+  const cliApplication = new CLIApplication();
+  cliApplication.registerCommands([
+    new HelpCommand(),
+    new VersionCommand()
+  ]);
+
+  cliApplication.processCommand(process.argv);
+}
+
+bootstrap();
